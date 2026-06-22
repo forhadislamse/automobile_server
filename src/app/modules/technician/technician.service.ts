01c9b03885aed78729a1c60f19c20f14bb093d7d
Muhammad Forhad Islam <i.rforhad@gmail.com>
2026-06-22 22:07:19 +0600

    fix technician email sender

--- src/app/modules/technician/technician.service.ts
+++ src/app/modules/technician/technician.service.ts
@@ -4,8 +4,8 @@ import * as bcrypt from 'bcrypt';
 import { startOfWeek, endOfWeek, startOfMonth, endOfMonth, eachDayOfInterval, format } from 'date-fns';
 import prisma from '../../../shared/prisma';
 import ApiError from '../../../errors/ApiErrors';
-// import emailSender from '../../../shared/brevoMailSender';
-import emailSender from '../../../shared/emailSender';
+import emailSender from '../../../shared/brevoMailSender';
+// import emailSender from '../../../shared/emailSender';
 import { TAddTechnician } from './technician.interface';
 import { technicianInvitationTemplate } from './technician.utils';
 
